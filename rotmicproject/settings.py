9b286855e4e8e555fd977b06a5671a1b5d9c7c12
Raik Gruenberg <raik@iric.fake>
2013-06-30 18:33:24 -0400

    add app and initial migration

--- rotmicproject/settings.py
+++ rotmicproject/settings.py
@@ -120,11 +120,12 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south'
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
+    'rotmic',
 )
 
 # A sample logging configuration. The only tangible logging
