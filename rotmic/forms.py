1f8724a67d381f62f08a1f20d13006534dea2e7e
Raik Gruenberg <raik@iric>
2013-08-19 18:23:55 -0400

    pre-populate Category field if form is called with existing object; set initial values to Plasmid / generic plasmid

--- rotmic/forms.py
+++ rotmic/forms.py
@@ -4,21 +4,31 @@ from rotmic.models import DnaComponent, DnaComponentType
 
 
 class DnaComponentForm(forms.ModelForm):
+    
     componentCategory = forms.ModelChoiceField(label='Category',
                                                queryset=DnaComponentType.objects.filter(subTypeOf=None),
                                                required=True, 
-                                               empty_label=None)
+                                               empty_label=None,
+                                               initial=DnaComponentType.objects.get(name='Plasmid').id)
+
     componentType = forms.ModelChoiceField(label='Type',
                                            queryset=DnaComponentType.objects.exclude(subTypeOf=None),
-                                           required=True, 
+                                           required=True,
+                                           initial=DnaComponentType.objects.get(name='generic plasmid').id,
                                            widget=forms.RadioSelect, empty_label=None)
-##    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 4}),required=False)
-    sequence = forms.CharField(widget=forms.Textarea(attrs={'cols': 80, 'rows': 4}),required=False)
     
     
     def __init__(self, *args, **kwargs):
-        self.request = kwargs.pop('request', None)
         super(DnaComponentForm, self).__init__(*args, **kwargs)
+        self.request = kwargs.pop('request', None)
+
+        o = kwargs.get('instance', None)
+        if o:
+            self.fields['componentCategory'].initial = o.componentType.subTypeOf
+        
                 
     class Meta:
-        model = DnaComponent 
+        model = DnaComponent
+        widgets = {  ## customize widget dimensions
+            'sequence' : forms.Textarea(attrs={'cols': 80, 'rows': 4}) 
+        }
