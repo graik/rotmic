0ee93445d5fd00eb6f7139d8dbbbb32b835323f8
Raik Gruenberg <raik@iric>
2013-08-21 15:15:44 -0400

    fix database location so that it is also found from normal python shell

---
+++ rotmic/utils/fileutils.py
@@ -0,0 +1,47 @@
+import os.path as osp
+
+def absfile( filename, resolveLinks=1 ):
+    """
+    Get absolute file path::
+      - expand ~ to user home, change
+      - expand ../../ to absolute path
+      - resolve links
+      - add working directory to unbound files ('ab.txt'->'/home/raik/ab.txt')
+
+    @param filename: name of file
+    @type  filename: str
+    @param resolveLinks: eliminate any symbolic links (default: 1)
+    @type  resolveLinks: 1|0
+    
+    @return: absolute path or filename
+    @rtype: string
+
+    @raise ToolsError: if a ~user part does not translate to an existing path
+    """
+    if not filename:
+        return filename
+    r = osp.abspath( osp.expanduser( filename ) )
+
+    if '~' in r:
+        raise ToolsError, 'Could not expand user home in %s' % filename
+
+    if resolveLinks:
+        r = osp.realpath( r )
+    r = osp.normpath(r)
+    return r
+
+def approot():
+    """
+    Root of labrack project.
+    
+    @return: absolute path of the root of current django app::
+             i.e. '/home/py/rotmicproject/'
+    @rtype: string
+    """
+    ## import this module
+    #from labrack import tools
+    ## get location of this module
+    f = absfile(__file__)
+    ## extract path and assume it is 'project_root/Biskit'
+    f = osp.split( f )[0] + '/../../'
+    return absfile( f )
--- rotmicsite/settings.py
+++ rotmicsite/settings.py
@@ -1,5 +1,8 @@
 # Django settings for rotmicsite project.
 
+import rotmic.utils.fileutils as F
+import os.path as osp
+
 DEBUG = True
 #TEMPLATE_DEBUG = DEBUG
 TEMPLATE_DEBUG = True
@@ -13,7 +16,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'rotmicdev.db',                      # Or path to database file if using sqlite3.
+        'NAME': osp.join(F.approot(), 'rotmicdev.db'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
