graik/rotmic

commit 4772d39bcc41f0091f7295a20243bf6ab636ac86

cosmetics and fix Amount Unit restriction in sample form

rotmic/forms.py
@@ -2,7 +2,7 @@ import os
 
 import django.forms as forms
 import django.db.models as models
-from django.db.models.query import QuerySet as Q
+from django.db.models import Q
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 

rotmic/models/attachments.py
@@ -91,8 +91,6 @@ def auto_delete_file_on_change(sender, instance, **kwargs):
 
 class ComponentAttachment(Attachment):
     parent_class = 'Component'
-    valid_extensions = ()  ## disable extension checking    
-    
     
     parent = models.ForeignKey(parent_class, related_name='attachments')
     
@@ -110,7 +108,6 @@ models.signals.pre_save.connect(auto_delete_file_on_change,
 
 class SampleAttachment(Attachment):
     parent_class = 'Sample'
-    valid_extensions = ()  ## disable extension checking
     
     parent = models.ForeignKey(parent_class, related_name='attachments')
 

rotmic/models/components.py
@@ -18,7 +18,6 @@ from datetime import datetime
 import re
 
 from django.db import models
-from django.db.models.query import QuerySet as Q
 from django.utils.safestring import mark_safe
 
 from django.contrib.auth.models import User

rotmic/models/samples.py
@@ -18,6 +18,7 @@ import re
 
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models import Q
 
 from rotmic.models.components import UserMixin
 
@@ -66,7 +67,8 @@ class Sample( UserMixin ):
                                    verbose_name='Unit',
                                    related_name='amountUnit+', ## suppress back-reference
                                    null=True, blank=True, 
-                                   limit_choices_to = {'unitType': ['volume','number', 'mass']})
+                                   limit_choices_to = Q(unitType__in=['volume','number', 'mass'])
+                                   )
     
 
     def commentText(self):

commit 05c2386af4355d132c655bfa0155ba99ed38e337

fix form saving error that was introduced one day ago in commit 4772d39bcc41f0091f7295a20243bf6ab636ac86

rotmic/forms.py
@@ -17,7 +17,7 @@ import os
 
 import django.forms as forms
 import django.db.models as models
-from django.db.models import Q
+from django.db.models.query import QuerySet
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 
@@ -160,7 +160,7 @@ class DnaComponentForm(forms.ModelForm):
             data['vectorBackbone'] = None
         
         if category not in [T.dcVectorBB, T.dcFragment] and 'marker' in data:
-            data['marker'] = Q()
+            data['marker'] = QuerySet()
             
         ## validate that a vector backbone is given if category == Plasmid
         if category == T.dcPlasmid and not data.get('vectorBackbone',None):