cb6150b74948eaacb4e1edcce6d2c7ba286924df
Raik Gruenberg <raik@iric>
2013-10-15 17:46:40 -0400

    cosmetics

--- rotmicsite/urls.py
+++ rotmicsite/urls.py
@@ -9,16 +9,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rotmicsite.views.home', name='home'),
-    # url(r'^rotmicsite/', include('rotmicsite.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-    # url(r'^rotmic/dna/(?P<displayId>.*)/$',view_dnacomponent,name='dna'),
     url(r'^getTypeDnaInfo/(?P<maintype>.*)/$',getTypeDnaInfo,name='getTypeDnaInfo'),
     url(r'^getCellTypes/(?P<maintype>.*)/$',getCellTypes,name='getCellTypes'),
 
