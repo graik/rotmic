0d420a7c2e81b9b707ffd836e865ad2c742c1c74
Raik Gruenberg <raik@iric.fake>
2014-03-16 21:31:07 -0400

    PC upload URLs

--- rotmicsite/urls.py
+++ rotmicsite/urls.py
@@ -39,11 +39,13 @@ urlpatterns = patterns('',
     url(r'^selectable/', include('selectable.urls')),
 
     url(r'^rotmic/dnacomponent/(?P<pk>.*)/genbank/$',V.view_genbankfile,name='genbankfile'),
+    url(r'^rotmic/proteincomponent/(?P<pk>.*)/genbank/$',V.view_genbankfile_aa,name='genbankfile_aa'),
 
     url(r'^rotmic/upload/dna/$', V.DnaXlsUploadView.as_view(), name='upload_dnacomponent'),
     url(r'^rotmic/upload/cell/$', V.CellXlsUploadView.as_view(), name='upload_cellcomponent'),
     url(r'^rotmic/upload/oligo/$', V.OligoXlsUploadView.as_view(), name='upload_oligocomponent'),
     url(r'^rotmic/upload/chemical/$', V.ChemicalXlsUploadView.as_view(), name='upload_chemicalcomponent'),
+    url(r'^rotmic/upload/protein/$', V.ProteinXlsUploadView.as_view(), name='upload_proteincomponent'),
 
     url(r'^rotmic/upload/location/$', V.LocationXlsUploadView.as_view(), name='upload_location'),
     url(r'^rotmic/upload/rack/$', V.RackXlsUploadView.as_view(), name='upload_rack'),
@@ -53,6 +55,7 @@ urlpatterns = patterns('',
     url(r'^rotmic/upload/oligosample/$', V.OligoSampleXlsUploadView.as_view(), name='upload_oligosample'),
     url(r'^rotmic/upload/chemicalsample/$', V.ChemicalSampleXlsUploadView.as_view(), name='upload_chemicalsample'),
     url(r'^rotmic/upload/cellsample/$', V.CellSampleXlsUploadView.as_view(), name='upload_cellsample'),
+    url(r'^rotmic/upload/proteinsample/$', V.ProteinSampleXlsUploadView.as_view(), name='upload_proteinsample'),
 
     url(r'^', include(admin.site.urls)),
 )
