2308ab4b897a8a06efe2753358bd3c10b0108fe2
Raik Gruenberg <raik@iric.fake>
2014-03-25 17:51:19 -0400

    set site-wide default for date format and disable locales

--- rotmicsite/settings.py
+++ rotmicsite/settings.py
@@ -47,10 +47,15 @@ USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = True
+USE_L10N = False
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
+
+# site-wide default for displaying dates
+DATE_FORMAT = 'Y-m-d'
+
+DATETIME_FORMAT = 'Y-m-d H:i'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
