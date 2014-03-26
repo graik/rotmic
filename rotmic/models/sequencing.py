d2ab571599acada39b109a3afd1aaf3b522649ee
Raik Gruenberg <raik@iric.fake>
2014-03-26 10:43:06 -0400

    cosmetics

--- rotmic/models/sequencing.py
+++ rotmic/models/sequencing.py
@@ -13,7 +13,7 @@
 ## GNU Affero General Public License for more details.
 ## You should have received a copy of the GNU Affero General Public
 ## License along with rotmic. If not, see <http://www.gnu.org/licenses/>.
-
+"""Models for attaching sequencing information to samples"""
 from datetime import datetime
 
 from django.db import models
