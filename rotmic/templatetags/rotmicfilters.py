6fe08e30c2d49093f9ea45d145ae8b0780e8b0f7
Raik Gruenberg <raik@iric.fake>
2014-04-05 23:02:16 -0400

    remove ratedcomments tags

--- rotmic/templatetags/rotmicfilters.py
+++ rotmic/templatetags/rotmicfilters.py
@@ -128,31 +128,3 @@ def get_verbose_short(object):
         return r
     return r.split()[0]
 
-
-@register.filter(name='restyle')
-def restyle(field, css):
-    """
-    from: http://vanderwijk.info/blog/adding-css-classes-formfields-in-django-templates/
-    Add additional CSS or style tags to a django-rendered field directly in template.
-    
-    Usage: {{ form.field | restyle:"text-align:right" }}
-    """
-    attrs = {}
-    definition = css.split(',')
-
-    for d in definition:
-        if ':' not in d:
-            attrs['class'] = d
-        else:
-            t, v = d.split(':')
-            attrs[t] = v
-            
-    return field.as_widget(attrs=attrs)
-
-
-@register.assignment_tag
-def recentcomments(n=10):
-    qs = ratedcomments.get_model().objects.filter(
-            is_removed = False,
-        )
-    return qs.order_by('-submit_date')[:n]
