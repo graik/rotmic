cb3e6963a7a192288514ff2926a0211b05eaf312
Raik Gruenberg <raik@iric.fake>
2014-04-07 16:59:42 -0400

    remove guardian app -- step 2

--- rotmicsite/settings_dev.py
+++ rotmicsite/settings_dev.py
@@ -133,10 +133,8 @@ TEMPLATE_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
 )
 
-# configure for django-guardian permission system
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend', # default
-    'guardian.backends.ObjectPermissionBackend',
 )
 ANONYMOUS_USER_ID = -1
 
@@ -152,7 +150,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
-    'guardian',
     'selectable',
     'reversion',
     'rotmic',
