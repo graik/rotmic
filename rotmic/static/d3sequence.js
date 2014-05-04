06ee8f606d4072f7ee11a73272b49fd876dcf1e0
Raik Gruenberg <raik@iric.fake>
2014-05-03 20:31:09 -0400

    documentation

--- rotmic/static/d3sequence.js
+++ rotmic/static/d3sequence.js
@@ -1,7 +1,38 @@
+/** Rotten Microbes (rotmic) -- Laboratory Sequence and Sample Management
+## Copyright 2013 - 2014 Raik Gruenberg
+
+## This file is part of the rotmic project (https://github.com/graik/rotmic).
+## rotmic is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Affero General Public License as
+## published by the Free Software Foundation, either version 3 of the
+## License, or (at your option) any later version.
+
+## rotmic is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Affero General Public License for more details.
+## You should have received a copy of the GNU Affero General Public
+## License along with rotmic. If not, see <http://www.gnu.org/licenses/>.
+**/
 /** Simple Sequence + Annotation display widget using D3 
 * requires CSS:
 * - axis ... sequence ruler with regular position marks
 * - feature-label ... text properties for annotation labels
+*
+* Usage:
+* <div id='containerElementId'></div>
+* seqdisplay.init('containerElementID');
+* seqdisplay.load(sequence, features);
+*
+* Features should look like this:
+[
+ { 'name': "name",
+   'color': "#FFFFFF",
+   'start': 1,
+   'end': 100,
+   'strand' : 1,
+   'type' : "CDS" },
+]
 **/
 
 
