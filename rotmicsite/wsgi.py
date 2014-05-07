c5d4f8a901f76755b7b2fe3199d3d0b5c886247c
Raik Gruenberg <raik@iric.fake>
2014-05-07 11:32:13 -0400

    document apache setting requirements

--- rotmicsite/wsgi.py
+++ rotmicsite/wsgi.py
@@ -23,9 +23,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rotmicsite.settings")
 
 # support user upload of files with non-ascii file names
 # see: https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/modwsgi/#if-you-get-a-unicodeencodeerror
-# os.environ.setdefault("LANG", "en_US.UTF-8")
-# os.environ.setdefault("LC_ALL", "en_US.UTF-8")
-os.environ['LANG'] = "en_US.UTF-8"  ## hard override of APACHE settings in /etc/apache2/envvars
+# NOTE: neither of these variants is doing the trick if LANG
+#       is defined wrongly in /etc/apache2/envvars (which it usually is)
+#       apparently, envvars is overriding LANG *after* it has been set by wsgi.py 
+os.environ['LANG'] = "en_US.UTF-8"     ## hard override of systems default
 os.environ['LC_ALL'] = "en_US.UTF-8"
 
 # This application object is used by any WSGI server configured to use this
