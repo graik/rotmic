1f32236127c86b09169b1f6e5ffdd306de556b6d
graik <raik.gruenberg@gmail.com>
2014-06-15 19:13:47 -0400

    basic search form with JQuery calendar widget

--- rotmic/searchforms.py
+++ rotmic/searchforms.py
@@ -15,12 +15,42 @@
 ## License along with rotmic. If not, see <http://www.gnu.org/licenses/>.
 import django_filters as F
 
+import django.forms as forms
+
 import rotmic.models as M
 
-class DnaComponentFilter(F.FilterSet):
+class JQueryUIDatepickerWidget(forms.DateInput):
+    def __init__(self, **kwargs):
+        super(forms.DateInput, self).__init__(attrs={"size":10, "class": "dateinput"}, **kwargs)
+
+    class Media:
+        css = {"all":("http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/themes/redmond/jquery-ui.css",)}
+##        js = ("http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js",
+##              "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js",)
+
+class ComponentFilter(F.FilterSet):
     
-    displayId = F.CharFilter(label='ID', lookup_type='contains')
+    displayId = F.CharFilter(label='ID', lookup_type='contains',
+                             help_text='(part of unique ID)')
+
     name      = F.CharFilter(label='Name', lookup_type='contains')
+    
+    
+
+    registeredAt = F.DateTimeFilter(label='Registered before',
+                                    widget=JQueryUIDatepickerWidget,
+                                    lookup_type='lte')
+    
+    modifiedAt = F.DateFilter(label='Modified after',
+                                widget=JQueryUIDatepickerWidget,
+                                lookup_type='gte')
+
+    filterfields = ['displayId', 'name', 'status',
+                  'registeredBy', 'registeredAt',
+                  'modifiedBy','modifiedAt',]
+    
+
+class DnaComponentFilter(ComponentFilter, F.FilterSet):
     
     insertId  = F.CharFilter(name='insert__displayId', label='insert ID',
                              lookup_type='contains')
@@ -35,8 +65,7 @@ class DnaComponentFilter(F.FilterSet):
 
     class Meta:
         model = M.DnaComponent
-        fields = ['displayId', 'name', 'status',
-                  'modifiedBy','modifiedAt',]
+        fields = ComponentFilter.filterfields
 
     def __init__(self, *args, **kwargs):
         super(DnaComponentFilter, self).__init__(*args, **kwargs)
