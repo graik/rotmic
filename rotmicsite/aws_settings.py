17b3253911873f66cce0a2d1a17ebcfb657ae6f9
graik <graik@users.noreply.github.com>
2016-05-09 23:06:54 +0300

    try another cryptic setting

--- rotmicsite/aws_settings.py
+++ rotmicsite/aws_settings.py
@@ -1,6 +1,6 @@
 import os
 
-##AWS_QUERYSTRING_AUTH = False
+AWS_QUERYSTRING_AUTH = False
 
 AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
