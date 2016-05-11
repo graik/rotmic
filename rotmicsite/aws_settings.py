a4ec5e699b83ab4aec03a231b40b7e03e47afa72
graik <graik@users.noreply.github.com>
2016-05-11 18:25:58 +0300

    rename env variables for clarity

--- rotmicsite/aws_settings.py
+++ rotmicsite/aws_settings.py
@@ -6,8 +6,8 @@ AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
         'Cache-Control': 'max-age=6000',
     }
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID', '')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_ID_KEY', '')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_ACCESS_SECRET_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', '')
 
 S3_URL = 'https://%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
